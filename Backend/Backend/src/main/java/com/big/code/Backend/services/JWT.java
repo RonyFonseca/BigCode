71b7128c3390536e41d4e25d89bad77b16072a3e
Rony_Fonseca <ronyfonsecadelima24@gmail.com>
2025-10-17 23:01:42 -0300

    refactor(Token): Mudança de secretkey.

--- Backend/Backend/src/main/java/com/big/code/Backend/services/JWT.java
+++ Backend/Backend/src/main/java/com/big/code/Backend/services/JWT.java
@@ -3,6 +3,8 @@ package com.big.code.Backend.services;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -11,8 +13,16 @@ import java.util.Date;
 
 @Component
 public class JWT {
-    private final String SECRET = "awsdawdsjawnsdawd6564dsa53s3a2s321a5a3sdasdawsdagwfsgd";
-    private final Key secretKey = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    @Value("${jwt.secret}")
+    private String SECRET;
+
+    private Key secretKey;
+
+    @PostConstruct
+    public void init(){
+        secretKey = Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
 
     public String generateToken(String email){
         return Jwts.builder().setSubject(email).setIssuedAt(new Date()).signWith(secretKey).compact();

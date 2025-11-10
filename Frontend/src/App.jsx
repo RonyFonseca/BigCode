413bb5e39a52746f8d59f1349f579ad1f2d7096f
Rony_Fonseca <ronyfonsecadelima24@gmail.com>
2025-11-10 13:01:01 -0300

    feat(Constantes): Implementação das constantes iniciais do css.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"; 
 
+//----------Paginas-----------
+import Login from "./Pages/Auth/Login.jsx"; 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/Pages/Auth/Login.jsx
@@ -0,0 +1,11 @@
+
+function Login(){
+    return(
+        <>
+            <h1>Login</h1>
+            <h2>Login</h2>
+        </>
+    )
+}
+
+export default Login; 
